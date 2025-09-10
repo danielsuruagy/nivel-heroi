b4b0911a5f1ff3737a5ad9483e72e17b9e711d69
Daniel Suruagy <daniel.suruagy@outlook.com>
2025-09-10 13:39:24 -0300

    Subindo o Projeto

---
+++ index.js
@@ -0,0 +1,24 @@
+let nomeHeroi = "Daniel"
+let xpHeroi = 1
+
+let eloHeroi
+
+if (xpHeroi < 1000) {
+    eloHeroi = "Ferro"
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    eloHeroi = "Bronze"
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    eloHeroi = "Prata"
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    eloHeroi = "Ouro"
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    eloHeroi = "Platina"
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    eloHeroi = "Ascendente"
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    eloHeroi = "Imortal"
+} else {
+    eloHeroi = "Radiante"
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nível de: ${eloHeroi}`)
